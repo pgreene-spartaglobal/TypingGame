9dabe57f081be0d7708afa5523cf6898c52ee815
Philip Greene <PGreene@spartaglobal.com>
2019-09-09 12:21:28 +0100

    Implemented basic score and lives system

--- TypingGame/MainWindow.xaml.cs
+++ TypingGame/MainWindow.xaml.cs
@@ -23,13 +23,17 @@ namespace TypingGame
     {
         WordManager wordManager = new WordManager();
         Random rnd = new Random();
+        int totalScore = 0;
+        int lives = 10;
 
         public MainWindow()
         {
             InitializeComponent();
             InitializeTimer(); // Create timer used for 'Update' method
 
-            //wordManager.AddWord();
+            wordManager.AddWord();
+            UpdateScore(totalScore);
+            UpdateLives(lives);
         }
 
         private void InitializeTimer()
@@ -45,10 +49,30 @@ namespace TypingGame
         private void dt_Update(object sender, EventArgs e)
         {
             wordManager.AddWord();
-            foreach (Word word in wordManager.words)
+
+            for (int i = 0; i < wordManager.words.Count; i++)
             {
-                Canvas.SetTop(word.text, Canvas.GetTop(word.text) + word.fallSpeed);
+                Canvas.SetTop(wordManager.words[i].text, Canvas.GetTop(wordManager.words[i].text) + wordManager.words[i].fallSpeed);
+                if (Canvas.GetTop(wordManager.words[i].text) > Canv.ActualHeight)
+                {
+                    lives--;
+                    UpdateLives(lives);
+                    RemoveWordCanvas(wordManager.words[i].text);
+                    wordManager.RemoveWord(wordManager.words[i]);
+                }
             }
+
+            //foreach (Word word in wordManager.words)
+            //{
+            //    Canvas.SetTop(word.text, Canvas.GetTop(word.text) + word.fallSpeed);
+            //    if (Canvas.GetTop(word.text) > Canv.ActualHeight)
+            //    {
+            //        lives--;
+            //        UpdateLives(lives);                    
+            //        RemoveWordCanvas(word.text);
+            //        wordManager.RemoveWord(word);
+            //    }
+            //}
         }
 
         private void Window_TextInput(object sender, TextCompositionEventArgs e)
@@ -57,7 +81,7 @@ namespace TypingGame
             wordManager.TypeLetter(e.Text[0]);
         }
 
-        public void SpawnWord(TextBlock text)
+        public void SpawnWordCanvas(TextBlock text)
         {
             double minValue = 0;
             double maxValue = Canv.ActualWidth;
@@ -68,20 +92,32 @@ namespace TypingGame
             Canv.Children.Add(text);
         }
 
-        public void RemoveWord(TextBlock text)
+        public void RemoveWordCanvas(TextBlock text)
         {
             Canv.Children.Remove(text);
+        }
+
+        public void UpdateScore(int score)
+        {
+            totalScore += score;
+            Score.Content = "Score: " + totalScore;
+        }
+
+        public void UpdateLives(int lives)
+        {
+            Lives.Content = "Lives: " + lives;
         }
     }
 
 
     public class WordManager
     {
-
+        MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
         public List<Word> words = new List<Word>();
 
         private bool hasActiveWord;
         private Word activeWord;
+        private int scoreToAdd = 0;
 
         public void AddWord()
         {
@@ -91,6 +127,11 @@ namespace TypingGame
             words.Add(word);
         }
 
+        public void RemoveWord(Word word)
+        {
+            words.Remove(word);
+        }
+
         public void TypeLetter(char letter)
         {
             if (hasActiveWord)
@@ -98,6 +139,7 @@ namespace TypingGame
                 if (activeWord.GetNextLetter() == letter)
                 {
                     activeWord.TypeLetter();
+                    scoreToAdd++;
                 }
             }
             else
@@ -118,30 +160,33 @@ namespace TypingGame
             {
                 hasActiveWord = false;
                 words.Remove(activeWord);
+                mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+                mainWindow.UpdateScore(scoreToAdd);
+                scoreToAdd = 0;
             }
         }
     }
 
-    public class Word : Window
+    public class Word
     {
         public TextBlock text = new TextBlock();
         public string wordValue;
         private int typeIndex;
         public int fallSpeed = 12;
 
-        MainWindow mainWindow = (MainWindow)Application.Current.MainWindow;
+        MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
 
         public Word(string word)
         {
             this.wordValue = word;
             typeIndex = 0;
             text.Text = word;
-            text.FontSize = 28;
+            text.FontSize = 24;
             text.Foreground = Brushes.MintCream;
-            text.Background = Brushes.Black;
-            text.FontWeight = FontWeights.Light;
+            text.Background = new SolidColorBrush(Color.FromArgb(0xBF, 0x00, 0, 0));
+            text.FontWeight = FontWeights.Thin;
             text.Padding = new Thickness(5);
-            mainWindow.SpawnWord(text);
+            mainWindow.SpawnWordCanvas(text);
         }
 
         public char GetNextLetter()
@@ -162,7 +207,7 @@ namespace TypingGame
             if (wordTyped)
             {
                 // remove word
-                mainWindow.RemoveWord(text);
+                mainWindow.RemoveWordCanvas(text);
             }
 
             return wordTyped;
